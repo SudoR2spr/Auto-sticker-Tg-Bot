1fdbfeaa8fd7a892d4946d3568c30794b8faa3d6
WOODcraft <121882568+SudoR2spr@users.noreply.github.com>
2024-09-21 18:08:13 +0530

    Auto-Sticker-Tg-Bot
    Ohh Bhai

---
+++ stickers.js
@@ -0,0 +1,10 @@
+// By - WOODcraft @SudoR2spr
+// If you want to add sticker ID add here as per your choice
+
+export const stickerIds = [
+    'CAACAgUAAxkBAAEM1dtm7VDhaQ4ONczoBn0kMQlMDDnzcgACiQ8AAmB9uVangRxqcjm23zYE',
+    'CAACAgUAAxkBAAEM1d1m7VDswE7OdPF98Ac81be4_v_WsQACTA4AAsLOuFb14hgAAQzI-y02BA',
+    'CAACAgIAAxkBAAEM1c9m7UFdh-7tjFKJa_UuruNTuB_VSAACGgADwDZPE4LbsLU8BkFXNgQ',
+    'CAACAgIAAxkBAAEM0yZm6Xz0hczRb-S5YkRIck7cjvQyNQACCh0AAsGoIEkIjTf-YvDReDYE',
+    'CAACAgQAAxkBAAEMzLJm483R53GzjAHmbr3ms3iOFSlTqAADFAAClt1hUHmPWNO7Sd_YNgQ'
+];
